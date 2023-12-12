0c53e5f52e9c7bd9781389cf361d2475dd7e798f
Rosalyn Wimberly <rosalynwimberly@gmail.com>
2023-12-11 20:08:37 -0500

    feat: Add countdown timer and format timer

--- Features/Timer.tsx
+++ Features/Timer.tsx
@@ -2,23 +2,40 @@ import { Button, FlatList, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 
 export default function Timer() {
-  const [seconds, setSeconds] = useState(0);
+  //Timer to log runs and intervals
+  const [milliseconds, setMilliseconds] = useState(0);
   const [isActive, setIsActive] = useState(false);
+  //Countdown clock before timer starts
+  const [countdown, setCountdown] = useState(5);
 
   useEffect(() => {
     let interval: string | number | NodeJS.Timeout | undefined;
 
-    if (isActive) {
+    if (isActive && countdown > 0) {
       interval = setInterval(() => {
-        setSeconds((prevSeconds) => prevSeconds + 1);
+        //countdown from 5 every second
+        setCountdown((prevCountdown) => prevCountdown - 1);
       }, 1000);
-    } else {
+    } else if (countdown === 0) {
       clearInterval(interval);
     }
 
     return () => clearInterval(interval);
-    
-  }, [isActive]);
+  }, [isActive, countdown]);
+
+  useEffect(() => {
+    let interval: string | number | NodeJS.Timeout | undefined;
+
+    if (isActive && countdown === 0) {
+      interval = setInterval(() => {
+        setMilliseconds((prevMilliseconds) => prevMilliseconds + 100);
+      }, 100);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, countdown]);
+
 
   const handleStart = () => {
     setIsActive(true);
@@ -26,17 +43,28 @@ export default function Timer() {
 
   const handleStop = () => {
     setIsActive(false);
-    setSeconds(0);
+    setMilliseconds(0);
+    setCountdown(5);
   };
 
-  const handlePause =() => {
+  const handlePause = () => {
     setIsActive(false);
+  };
+
+  const formatTime = (time: number) => {
+    return ('0' + Math.floor(time / 60000)).slice(-2) + ':' +
+           ('0' + Math.floor((time / 1000) % 60)).slice(-2) + ':' +
+           ('0' + ((time / 100) % 10).toFixed(0)).slice(-2);
   };
 
   return (
     <View>
-      <Text>Timer: {seconds} seconds</Text>
-      <Button title={isActive ? 'Pause' : 'Start'} onPress={isActive ? handlePause : (handleStart)} />
+      
+        <Text>Countdown: {countdown} seconds</Text>
+  
+        <Text>Timer: {formatTime(milliseconds)}</Text>
+  
+      <Button title={isActive ? 'Pause' : 'Start'} onPress={isActive ? handlePause : handleStart} />
       <Button title='Stop' onPress={handleStop} />
     </View>
   )
